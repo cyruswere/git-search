d72046827d68ef0dbac4b82ef39fb207ed9ff23d
cyruswere <cyrus.were@student.moringaschool.com>
2021-07-24 12:58:56 +0300

    Made changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  // apiUrl :'https://api.github.com/users/',
-  // gitToken: '?access_token=76a98e05315e9a2ad9a4055b261fcbf8241221a3 '
+  apiUrl :'https://api.github.com/users/',
+  gitToken: '?access_token=76a98e05315e9a2ad9a4055b261fcbf8241221a3 '
 };
 /*
  * For easier debugging in development mode, you can import the following file
